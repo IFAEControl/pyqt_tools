b15a87b88b79e0707f3eb506a76980fccbd92894
David Roman <droman@ifae.es>
2018-05-16 16:01:01 +0200

    Rename folder

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pyqt-tools',
+    packages = find_packages(),
+    platform=['any'],
+    version = '0.0.1',
+    author = 'David Roman',
+    author_email = 'davidroman96@gmail.com',
+    url = 'https://mumei.space/david/pyqt-tools'
+)
