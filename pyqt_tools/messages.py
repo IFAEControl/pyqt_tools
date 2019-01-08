8cf854b5abdab5664c4e99ab53b132734c826aa5
David Roman <droman@ifae.es>
2019-01-08 15:58:29 +0100

    remove guidata

--- pyqt_tools/messages.py
+++ pyqt_tools/messages.py
@@ -1,13 +1,14 @@
-import guidata
+import sys
+
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QMessageBox, QApplication
 
 
 def show_message(text, title, icon, buttons=QMessageBox.Ok):
     # We can not create multiple instances, so we check if an instance exists
     instance = QtCore.QCoreApplication.instance()
     if instance is None:
-        app = guidata.qapplication()
+        app = QApplication(sys.argv)
 
     msg = QMessageBox()
     msg.setIcon(icon)
